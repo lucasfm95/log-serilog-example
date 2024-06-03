aaa398a201dd273d19e0e5c4567d9901ade392c1
Lucas Martins <luucasmartins@hotmail.com>
2024-06-03 13:12:34 -0300

    first commit

---
+++ LogSerilog.Api/Program.cs
@@ -0,0 +1,16 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseSerilogRequestLogging();
+app.Run();
